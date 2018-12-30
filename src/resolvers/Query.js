966e337b42e6a8648d065ce62c1a479d9a2190f0
Andrew Locke <alocke12992@gmail.com>
2018-12-30 12:49:24 -0800

    adds filter to feed query

--- src/resolvers/Query.js
+++ src/resolvers/Query.js
@@ -1,7 +1,18 @@
-const feed = (parent, args, context, info) => {
-  return context.prisma.links()
-}
+const feed = async (parent, args, context) => {
+  const where = args.filter ? {
+    OR: [
+      { description_contains: args.filter },
+      { url_contains: args.filter },
+    ],
+  } : {};
+
+  const links = await context.prisma.links({
+    where,
+  });
+
+  return links;
+};
 
 module.exports = {
   feed,
-}
+};
