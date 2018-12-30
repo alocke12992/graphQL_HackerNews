402dae815973a46a5ec2069d92c968e1e2938464
Andrew Locke <alocke12992@gmail.com>
2018-12-30 10:11:35 -0800

    Functioning Auth

--- src/resolvers/Mutation.js
+++ src/resolvers/Mutation.js
@@ -1,12 +1,12 @@
-const bcrypt = require("bcrypt");
+const bcrypt = require("bcryptjs");
 const jwt = require("jsonwebtoken");
 const { APP_SECRET, getUserId } = require("../utils");
 
 async function signup(parent, args, context, info) {
 
   const password = await bcrypt.hash(args.password, 10);
-  const user = await context.prisma.createUser({...args, password})
-  const token = jwt.sign({ userID: user.id }, APP_SECRET)
+  const user = await context.prisma.createUser({ ...args, password })
+  const token = jwt.sign({ userId: user.id }, APP_SECRET)
 
   return {
     token,
