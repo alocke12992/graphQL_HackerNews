be5b3bf499fd2b433be3a5ee0b994c4ded45fd0d
Andrew Locke <alocke12992@gmail.com>
2018-12-30 12:00:22 -0800

    Adds eslint

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ const { GraphQLServer } = require("graphql-yoga");
 const { prisma } = require("./generated/prisma-client");
 const Query = require("./resolvers/Query");
 const Mutation = require("./resolvers/Mutation");
+const Subscription = require("./resolvers/Subscription");
 const User = require("./resolvers/User");
 const Link = require("./resolvers/Link");
 
