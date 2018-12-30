767e6be4d68c393cff5208874e1c2f801c522018
Andrew Locke <alocke12992@gmail.com>
2018-12-30 12:35:21 -0800

    Connect Vote resolver

--- src/index.js
+++ src/index.js
@@ -1,29 +1,30 @@
-require("dotenv").config()
-const { GraphQLServer } = require("graphql-yoga");
-const { prisma } = require("./generated/prisma-client");
-const Query = require("./resolvers/Query");
-const Mutation = require("./resolvers/Mutation");
-const Subscription = require("./resolvers/Subscription");
-const User = require("./resolvers/User");
-const Link = require("./resolvers/Link");
+require('dotenv').config();
+const { GraphQLServer } = require('graphql-yoga');
+const { prisma } = require('./generated/prisma-client');
+const Query = require('./resolvers/Query');
+const Mutation = require('./resolvers/Mutation');
+const Subscription = require('./resolvers/Subscription');
+const User = require('./resolvers/User');
+const Link = require('./resolvers/Link');
+const Vote = require('./resolvers/Vote');
 
 const resolvers = {
   Query,
   Mutation,
   Subscription,
   User,
-  Link
-}
+  Link,
+  Vote,
+};
 
 const server = new GraphQLServer({
-  typeDefs: "./src/schema.graphql",
+  typeDefs: './src/schema.graphql',
   resolvers,
-  context: request => { 
-    return {
-      ...request,
-      prisma,
-    }
-  },
-})
+  context: request => ({
+    ...request,
+    prisma,
+  }),
+});
 
-server.start(() => console.log(`Server is running at http://localhost:4000`))
+// eslint-disable-next-line no-console
+server.start(() => console.log('Server is running at http://localhost:4000'));
