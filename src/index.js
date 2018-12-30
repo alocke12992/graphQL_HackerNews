8d80bdfbe21c6efd540c816a9bb0c76b05392536
Andrew Locke <alocke12992@gmail.com>
2018-12-30 09:20:24 -0800

    Adds user model

--- src/index.js
+++ src/index.js
@@ -7,19 +7,21 @@ const resolvers = {
     feed: () => (root, args, context, info) => {
       return context.prisma.links()
     },
-  //   getLink: (parent, args, context, info) => {
-  //     let link = null
-  //     links.map(node => {
-  //       if (node.id === args.id){
-  //         link = node
-  //       }
-  //     })
-  //     if (!link){
-  //       return `link-${args.id} does not exist`
-  //     } else {
-  //       return link
-  //     }
-  //   }
+    // getLink: (parent, args, context, info) => {
+    //   let link = null
+    //   let links = context.prisma.links()
+    //   console.log(links)
+    //   // links.map(node => {
+    //   //   if (node.id === args.id){
+    //   //     link = node
+    //   //   }
+    //   // })
+    //   // if (!link){
+    //   //   return `link-${args.id} does not exist`
+    //   // } else {
+    //   //   return link
+    //   // }
+    // }
   },
   Mutation: {
     post: (root, args, context) => {
