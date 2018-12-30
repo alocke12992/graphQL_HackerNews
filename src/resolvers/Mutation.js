d041de7412a20ef138a7a047053861efba225892
Andrew Locke <alocke12992@gmail.com>
2018-12-30 12:26:29 -0800

    Cleans up Mutations.js per ESlint

--- src/resolvers/Mutation.js
+++ src/resolvers/Mutation.js
@@ -1,51 +1,67 @@
-const bcrypt = require("bcryptjs");
-const jwt = require("jsonwebtoken");
-const { getUserId } = require("../utils");
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+const { getUserId } = require('../utils');
 
-const signup = async (parent, args, context, info) => {
-
+const signup = async (parent, args, context) => {
   const password = await bcrypt.hash(args.password, 10);
-  const user = await context.prisma.createUser({ ...args, password })
-  const token = jwt.sign({ userId: user.id }, process.env.APP_SECRET)
+  const user = await context.prisma.createUser({ ...args, password });
+  const token = jwt.sign({ userId: user.id }, process.env.APP_SECRET);
 
   return {
     token,
-    user
-  }
-}
+    user,
+  };
+};
 
-const login = async (parent, args, context, info) => {
-  const user = await context.prisma.user({ email: args.email })
-  
+const login = async (parent, args, context) => {
+  const user = await context.prisma.user({ email: args.email });
   if (!user) {
-    throw new Error("No such user found")
+    throw new Error('No such user found');
   }
 
-  const valid = await bcrypt.compare(args.password, user.password)
+  const valid = await bcrypt.compare(args.password, user.password);
 
   if (!valid) {
-    throw new Error("Invalid password")
+    throw new Error('Invalid password');
   }
 
-  const token = jwt.sign({ userId: user.id}, process.env.APP_SECRET)
+  const token = jwt.sign({ userId: user.id }, process.env.APP_SECRET);
 
   return {
-    token, 
-    user
-  }
-}
+    token,
+    user,
+  };
+};
 
-const post = (root, args, context, info) => {
-  const userId = getUserId(context)
+const post = (root, args, context) => {
+  const userId = getUserId(context);
   return context.prisma.createLink({
     url: args.url,
     description: args.description,
-    postedBy: { connect: {id: userId }}
-  })
-}
+    postedBy: { connect: { id: userId } },
+  });
+};
+
+const vote = async (parent, args, context) => {
+  const userId = getUserId(context);
+  const linkExists = await context.prisma.$exists.vote({
+    user: { id: userId },
+    link: { id: args.linkId },
+  });
+
+  if (linkExists) {
+    throw new Error(`You've already voted for link: ${args.linkId}`);
+  }
+
+  return context.prisma.createVote({
+    user: { connect: { id: userId } },
+    link: { connect: { id: args.linkId } },
+  });
+};
 
 module.exports = {
   signup,
   login,
-  post
-}
+  post,
+  vote,
+};
