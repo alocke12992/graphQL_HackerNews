28448622b98d834ae041cf84ca350d47fdba069b
Andrew Locke <alocke12992@gmail.com>
2018-12-31 09:11:39 -0800

    Adds kirkham theme

--- client/src/components/Layout.jsx
+++ client/src/components/Layout.jsx
@@ -1,9 +1,18 @@
 import React from 'react';
 import Navbar from './Navbar';
+import { css } from '@emotion/core';
+import { rhythm } from '../utils/typography';
 
-export default ({children}) => (
-  <div>
+export default ({ children }) => (
+  <div
+  css={css`
+    margin: 0 auto;
+    max-width: 700px;
+    padding: ${rhythm(2)};
+    padding-top: ${rhythm(1.5)};
+  `}
+  >
     <Navbar />
     {children}
   </div>
-);
+)
